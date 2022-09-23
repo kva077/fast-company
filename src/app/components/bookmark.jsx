fc8485aadfae2d4b043eee81d1a87847caa251e3
Колесников Влад <vkolesnikov@softrust.ru>
2022-09-23 10:03:31 +0300

    proptypes v2

--- src/app/components/bookmark.jsx
+++ src/app/components/bookmark.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const BookMark = ({ status, ...rest }) => {
+const BookMark = ({ bookmark, ...rest }) => {
     const halfClass = "bi bi-bookmark";
     return (
-        <button onClick={() => rest.onToogleBookMark(rest._id)}>
-            <i className={status ? `${halfClass}-fill` : halfClass}></i>
+        <button {...rest}>
+            <i className={bookmark ? `${halfClass}-fill` : halfClass}></i>
         </button>
     );
 };
 BookMark.propTypes = {
-    status: PropTypes.bool.isRequired,
-    rest: PropTypes.object.isRequired
+    bookmark: PropTypes.bool.isRequired
 };
 
 export default BookMark;
